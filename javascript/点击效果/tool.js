ed82d3f93de2d42241f0cd88c31877ef35d68a22
qiaodaima <674059309@qq.com>
2018-04-02 00:47:00 +0800

    点击效果

---
+++ javascript/点击效果/tool.js
@@ -0,0 +1,66 @@
+function getMouseAxis(event) {
+  return {
+    x: event.clientX,
+    y: event.clientY
+  };
+}
+
+function createElement(tag) {
+  return document.createElement(tag ? tag : 'div');
+}
+
+function delSelfElement(element) {
+  element.parentNode.removeChild(element);
+}
+
+function setElementAttribute(element, value) {
+  Object.keys(value).forEach(function (key) {
+    switch (typeof value[key]) {
+      case 'string':
+        element[key] = value[key];
+        break;
+      default:
+        setElementAttribute(element[key], value[key]);
+        break;
+    }
+  });
+}
+
+function animate(element, styles, speed, callback) {
+  var elementCurrstyle = {};
+  var timer = null;
+  var count = parseInt(speed / 16.7);
+  var countTemp = count;
+  var step = {};
+
+  // 计算出每个样式的增量
+  Object.keys(styles).forEach(function (key) {
+    step[key] = (parseInt(styles[key]) - parseInt(getComputedStyle(element, null)[key])) / countTemp;
+  });
+
+  console.log(step);
+
+  timer = setInterval(function () {
+    if(count-- <= 0) {
+      callback && callback();
+      clearInterval(timer);
+    }
+
+    // 获取元素当前样式
+    Object.keys(styles).forEach(function (key) {
+      elementCurrstyle[key] = getComputedStyle(element, null)[key];
+    });
+
+    // 改变元素样式
+    Object.keys(styles).forEach(function (key) {
+      element.style[key] = parseInt(elementCurrstyle[key]) + step[key] + 'px';
+    });
+  }, 16.7);
+}
+
+
+function getRandomNumber(min, max) {
+  var temp = Math.random() * (max - min - 1) + max;
+
+  return Math.floor(temp);
+}
